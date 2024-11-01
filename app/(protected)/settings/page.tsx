dfb2fe63fa76c29deb453c946a7c8cedf6a5307d
bimengzhou <dylanmengzhou@gmail.com>
2024-11-01 23:24:35 +0900

    version 0.0.7: Complete the middleware & login section. Implement auth.js to the login behavior

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,21 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+const SettingsPage = async () => {
+  const session = await auth();
+  return (
+    <div className="">
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: "/auth/login", redirect: true });
+        }}
+      >
+        <Button type="submit">Sign Out</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SettingsPage;
---
+++ middleware.ts
@@ -0,0 +1,50 @@
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
+// Use only one of the two middleware options below
+// 1. Use middleware directly
+// export const { auth: middleware } = NextAuth(authConfig)
+
+// 2. Wrapped middleware option
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return undefined;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return undefined;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return undefined;
+});
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
