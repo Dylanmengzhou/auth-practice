5a2e950503798ae4c16b55b27cf40c253539e540
bimengzhou <dylanmengzhou@gmail.com>
2024-11-01 18:35:30 +0900

    version 0.0.6: Complete Create User. Add db behavior

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
-import {  RegisterSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
 import * as z from "zod";
+import { getUserByEmail } from "@/data/user";
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
@@ -8,5 +11,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // check if we have existing user
+  const existingUser = await getUserByEmail(email)
+
+  console.log(existingUser);
+
+  if (existingUser) {
+    return { error: "Email is already registered!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // Todo: Send varification token email
+
+  return { success: "User created!" };
 };
